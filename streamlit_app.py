7b0281bcbcb2f6f666e7b87f258adf09257143fe
SnowflakeSaravanan <136088608+SnowflakeSaravanan@users.noreply.github.com>
2023-06-09 19:48:22 +0530

    Update streamlit_app.py
    Added BF menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title('My Parents New Healthier Diner')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Idly')
+streamlit.teaxt('Dosa')
+streamlit.teaxt('Vada')
+streamlit.teaxt('Poori')
