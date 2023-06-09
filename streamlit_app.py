181668129cf31c8e66826dac6d454fe0bd4ac4b1
SnowflakeSaravanan <136088608+SnowflakeSaravanan@users.noreply.github.com>
2023-06-09 20:09:25 +0530

    Update streamlit_app.py
    Fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Idly')
-streamlit.teaxt('Dosa')
+streamlit.text('Dosa')
 streamlit.teaxt('Vada')
 streamlit.teaxt('Poori')
