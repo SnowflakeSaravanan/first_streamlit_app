b0ba6d9a3cf4f2401590499acea7481d1130e7d8
SnowflakeSaravanan <136088608+SnowflakeSaravanan@users.noreply.github.com>
2023-06-19 11:22:21 +0530

    Update streamlit_app.py
    Added try catch block

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,23 @@ streamlit.dataframe(my_fruit_list)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'apple')
+try:
+      fruit_choice = streamlit.text_input('What fruit would you like information about?', 'apple')
+if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+else:  
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 streamlit.write('The user entered', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+except URLError as e:
+  streamlit.error()
 
 #take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 #output it the screen as table
-streamlit.dataframe(fruityvice_normalized)
+
 
 #don't run anything past here while troubleshooting.
 streamlit.stop()
